ff818ef98c8b7affe09385c163787c46ba1ddc02
HIFJUR RAHMAN <hifjur1313@gmail.com>
2021-09-23 06:12:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+//rest counties is dead so I made it wiht mealdb food
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Meals></Meals>
     </div>
   );
 }
 
+function Meals(){
+  const [meals, setMeals] = useState([]);
+  useEffect(() => {
+    fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+    .then(res => res.json())
+    .then(data => setMeals(data.categories));
+  },[])
+  return(
+    <div>
+      <h2>Loading meals</h2>
+      <h4>Meals Catagory Available:{meals.length}</h4>
+      {
+        meals.map(meal => <Meal name={meal.strCategory } description={meal.strCategoryDescription } pic={meal.strCategoryThumb}></Meal>)
+      }
+    </div>
+  )
+}
+
+function Meal(props){
+  return(
+    <div>
+      <h2>{props.name}</h2>
+      <p>{props.description}</p>
+      <img src={props.pic} alt="" />
+    </div>
+  )
+}
 export default App;
